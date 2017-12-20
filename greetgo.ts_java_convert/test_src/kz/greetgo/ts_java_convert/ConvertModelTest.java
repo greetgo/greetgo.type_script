9c2caa969d2b83a94b31355717d4f335f22e0687
ekolpakov <ekolpakov@greet-go.com>
2017-12-20 14:08:44 +0600

    Сделан базовый тест ConvertModelTest.defineStructure_stringField

--- greetgo.ts_java_convert/test_src/kz/greetgo/ts_java_convert/ConvertModelTest.java
+++ greetgo.ts_java_convert/test_src/kz/greetgo/ts_java_convert/ConvertModelTest.java
@@ -1,5 +1,6 @@
 package kz.greetgo.ts_java_convert;
 
+import kz.greetgo.ts_java_convert.stru.ClassAttr;
 import kz.greetgo.ts_java_convert.stru.simple.SimpleTypeStr;
 import kz.greetgo.ts_java_convert.test_ConvertModel.ConvertModelDir;
 import org.testng.annotations.Test;
@@ -38,5 +39,32 @@ public class ConvertModelTest {
     assertThat(fr.classStructure.attrList.get(0).isArray).isFalse();
     assertThat(fr.classStructure.attrList.get(0).name).isEqualTo("field");
     assertThat(fr.classStructure.attrList.get(0).type.getClass().getName()).isEqualTo(SimpleTypeStr.class.getName());
+  }
+
+  @Test
+  public void defineStructure_stringField() throws Exception {
+
+    ConvertModelDir dir = new ConvertModelDir();
+    File class1 = dir.read("sub2/ClassWithStringField.ts");
+
+    TsFileReference fr = new TsFileReference(class1, "sub2", "ClassWithStringField");
+
+    ConvertModel convertModel = new ConvertModel(dir.sourceDir(), dir.destinationDir(), "kz.greetgo.wow");
+
+    assertThat(fr.classStructure).isNull();
+
+    convertModel.defineStructure(singletonList(fr));
+
+    assertThat(fr.classStructure).isNotNull();
+
+
+    assertThat(fr.classStructure.attrList).hasSize(1);
+
+    ClassAttr attr = fr.classStructure.attrList.get(0);
+
+    assertThat(attr.type.getClass().getName()).isEqualTo(SimpleTypeStr.class.getName());
+    assertThat(attr.isArray).isFalse();
+    assertThat(attr.name).isEqualTo("strField");
+    assertThat(attr.comment).isEqualTo(asList("  /**", "   * string field", "   * line 2 of comment", "   */"));
   }
 }
---
+++ greetgo.ts_java_convert/test_src/kz/greetgo/ts_java_convert/test_ConvertModel/sub2/ClassWithStringField.ts
@@ -0,0 +1,7 @@
+export class ClassWithStringField {
+  /**
+   * string field
+   * line 2 of comment
+   */
+  public strField: string;
+}
